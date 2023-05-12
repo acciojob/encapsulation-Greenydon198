9154f43c6140803c16c362b96e87c23d7b796168
Greeny Don <greenydon198@gmail.com>
2023-05-12 17:27:10 +0530

    Made changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        RWOnly ob = new RWOnly("Greeny");
+        ob.setS("GREENY");
+        System.out.println(ob.getS());
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String s;
+
+    public RWOnly(String s) {
+        this.s = s;
+        System.out.println(s);
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
 }
